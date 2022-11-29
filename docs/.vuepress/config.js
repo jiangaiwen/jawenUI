632f7ff148f570cf9d3838e508dc507067ae1a88
jiangaiwen <jawen.9@outlook.com>
2022-11-29 22:49:01 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+    title: 'Jawen-UI',
     base: '/jawenUI/',
     themeConfig: {
+        nav: [{
+            text: 'Home', link: '/'
+        }, {
+            text: 'Github', link: 'https://github.com/jiangaiwen/jawenUI'
+        }, {
+            text: 'Npm', link: 'https://www.npmjs.com/package/jawen-ui'
+        }],
         sidebar: [
             '/',
             '/componentDocs/card'
